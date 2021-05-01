69a1e1cae415b729ae19e888dad2a019e995ee08
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2021-05-01 12:28:27 +0000

    Add navbar and dropdown menu to framer motion

--- src/App.js
+++ src/App.js
@@ -3,34 +3,29 @@ import Layout from './components/navbar/Layout'
 import ImageSlider from './components/slider/ImageSlider'
 import { ImageData } from './components/slider/ImageData'
 
-
 import CardScreen from './components/framer_motion/advanced_animations/fade/CardScreen'
 import FramerMotionCards from './components/framer_motion/colours_and_fonts'
-
-
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <div style={{ minHeight: '100vh' }}>
-          <Switch>
-            <Route exact path='/'>
-              <div className="my-10">
-                <ImageSlider slides={ImageData} />
-              </div>
-            </Route>
-            <Route path='/carousel'>
+      <Layout />
+      <div style={{ minHeight: '100vh' }}>
+        <Switch>
+          <Route exact path='/carousel'>
+            <div className="my-10">
+              <ImageSlider slides={ImageData} />
+            </div>
+          </Route>
+          <Route path='/fm'>
+            <div className='my-5'>
+              <FramerMotionCards />
               <CardScreen />
-            </Route>
-            <Route path='/fm'>
-              <div className='my-5'>
-                <FramerMotionCards />
-              </div>
-            </Route>
-          </Switch>
-        </div>
-      </Layout>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+
     </Router>
 
   );
--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,33 +1,48 @@
-import { BrowserRouter as Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 export default function Header() {
+  const [framerMotionDropdownMenu, setFramerMotionDropdownMenu] = useState(false)
   return (
-    <>
+    <nav>
       <div className='flex items-center bg-indigo-700 w-screen h-full mb-4 justify-between'>
         {/* brand/logo */}
-        <div className='flex items-center mx-2 py-1'>
-          <img className='rounded-full shadow-2xl' src='https://esmaeilmirzaee.herokuapp.com/static/img/my__avatar.svg' width='64' height='64' />
-          <div className='mx-2'>
-            <p className='text-lg text-white font-bold'>Esmaeil MIRZAEE</p>
-            <p className='text-sm text-white font-light'>React Playground</p>
+        <a href='/'>
+          <div className='flex items-center mx-2 py-1 cursor-pointer'>
+            <img className='rounded-full shadow-2xl' src='https://esmaeilmirzaee.herokuapp.com/static/img/my__avatar.svg' width='64' height='64' />
+            <div className='mx-2'>
+              <p className='text-lg text-white font-bold'>Esmaeil MIRZAEE</p>
+              <p className='text-sm text-white font-light'>React Playground</p>
+            </div>
           </div>
-        </div>
+        </a>
         {/* nav items */}
         <div>
           <ul className='flex'>
-            <a href='/carousel'>
-              <li className='text-white mx-2 hover:border-b-2 hover:border-white hover:text-indigo-50 hover:scale-110 cursor-pointer'>
+            <li className='text-white mx-2 hover:border-b-2 hover:border-white hover:text-indigo-50 hover:scale-110 cursor-pointer'>
+              <Link to='/carousel'>
                 Carousel
+              </Link>
             </li>
-            </a>
-            <a href='/fm'>
-              <li className='text-white mx-2 hover:border-b-2 hover:border-white hover:text-indigo-50 hover:scale-110 cursor-pointer'>Framer Motion</li>
-            </a>
+            <li className='relative mx-2 text-white hover:border-b-2 hover:border-white hover:text-indigo-50 hover:scale-110 cursor-pointer' onMouseEnter={() => setFramerMotionDropdownMenu(true)} onMouseOut={() => setFramerMotionDropdownMenu(false)} onClick={() => setFramerMotionDropdownMenu(!framerMotionDropdownMenu)}>
+              <Link to='/fm'>
+                Framer Motion
+              </Link>
+              {framerMotionDropdownMenu && <ul className='absolute mt-2 w-48 bg-white rounded-md shadow-xl hover:rounded-md'>
+                <li className='block px-4 py-2 text-gray-600 hover:bg-indigo-600 hover:rounded-md hover:text-white '>
+                  <Link>Fade</Link>
+                </li>
+                <li className='block px-4 py-2 text-gray-600 hover:bg-indigo-600 hover:rounded-md hover:text-white '>
+                  <Link>Fade 1</Link>
+                </li>
+              </ul>}
+            </li>
           </ul>
 
         </div>
         {/* buttons */}
         <div></div>
       </div>
-    </>
+    </nav>
   )
 }
