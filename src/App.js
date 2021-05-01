8a693e78f87dbb6431732a6d29602d3e1efb4ad2
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2021-05-01 01:31:34 +0000

    ColorsAndFonts card sample

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import ImageSlider from './components/slider/ImageSlider'
 import { ImageData } from './components/slider/ImageData'
 
 import CardScreen from './components/framer_motion/CardScreen'
-
+import FramerMotionCards from './components/framer_motion/colours_and_fonts'
 function App() {
   return (
-    <>
+    <div style={{ minHeight: '100vh' }}>
       <div className="my-10">
         <ImageSlider slides={ImageData} />
       </div>
       <CardScreen />
-    </>
+
+      <div className='my-5'>
+        <FramerMotionCards />
+      </div>
+    </div>
   );
 }
 
