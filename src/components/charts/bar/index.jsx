736cd31cdd745d5bb30ec91a1c81ce9c2e03e826
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2021-07-22 12:53:52 +0000

    display bar chart perfectly fine

--- src/components/charts/bar/index.jsx
+++ src/components/charts/bar/index.jsx
@@ -2,36 +2,35 @@ import { useEffect, useRef } from 'react';
 import { select, scaleLinear, max, scaleBand, axisLeft, axisBottom } from 'd3';
 
 const render = (data, custom) => {
-  const xValue = (d) => +d.population.split(',').join('');
-  const yValue = (d) => d.name;
+  const xValue = (d) => d.name;
+  const yValue = (d) => +d.population.split(',').join('');
   const svg = select(custom.chart_name)
-    .attr('width', `${custom.width}`)
-    .attr('height', `${custom.height}`)
+    .attr(
+      'width',
+      `${custom.width + custom.margins.left + custom.margins.right}`,
+    )
+    .attr(
+      'height',
+      `${custom.height + custom.margins.top + custom.margins.bottom}`,
+    )
     .append('g')
     .attr(
       'transform',
-      `translate(${custom.margins.left}, ${custom.margins.top})`,
+      `translate(${custom.margins.top}, ${custom.margins.right})`,
     );
 
   // Scale
-  const xScale = scaleLinear()
-    .domain([0, max(data, xValue)])
+  const xScale = scaleBand()
+    .domain(data.map(xValue))
     .range([0, custom.width - custom.margins.left - custom.margins.right])
-    .nice();
-  const yScale = scaleBand()
-    .domain(data.map(yValue))
-    .range([0, custom.height - custom.margins.top - custom.margins.bottom])
     .padding(0.2);
+  const yScale = scaleLinear()
+    .domain([0, max(data, yValue) + custom.offset])
+    .range([custom.height - custom.margins.top - custom.margins.bottom, 0])
+    .nice();
 
   // 4. Define axis
   // Y Axis
-  // const yAxis = axisLeft(yScale);
-
-  // Use Y Axis
-  // The following line could be refactored as follows
-  // yAxis(svg.append('g'));
-
-  // Top two lines refactored as
   svg.append('g').call(axisLeft(yScale));
 
   // X Axis
@@ -41,7 +40,7 @@ const render = (data, custom) => {
     .attr(
       'transform',
       `translate(${0},${
-        custom.height - custom.margins.top - custom.margins.bottom
+        custom.height - custom.margins.left - custom.margins.right
       })`,
     );
 
@@ -51,9 +50,18 @@ const render = (data, custom) => {
     .data(data)
     .enter()
     .append('rect')
+    .attr('x', (d) => xScale(xValue(d)))
     .attr('y', (d) => yScale(yValue(d)))
-    .attr('width', (d) => xScale(xValue(d)))
-    .attr('height', yScale.bandwidth());
+    .attr(
+      'height',
+      (d) =>
+        custom.height -
+        custom.margins.top -
+        custom.margins.bottom -
+        yScale(yValue(d)),
+    )
+    .attr('width', xScale.bandwidth())
+    .classed('chart__bars', true);
 };
 
 export const BarChart = ({ data }) => {
@@ -70,6 +78,7 @@ export const BarChart = ({ data }) => {
       width: 900,
       height: 450,
       margins: { top: 100, right: 100, bottom: 100, left: 100 },
+      offset: 5,
     });
   }, []);
 
