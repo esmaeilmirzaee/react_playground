d406a7fc7f860f2de377a17fd6e85377ea486c28
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2021-07-26 10:33:57 +0000

    Simplee treemap

--- src/components/charts/index.jsx
+++ src/components/charts/index.jsx
@@ -1,5 +1,8 @@
 import { BarChart } from './bar/';
 import { TreeChart } from './map/Tree';
+import { TreeMapChart } from './map/TreeMap';
+
+// Static data
 import world_population from './data/world_population';
 import world_migrants from './data/world_migrants';
 import linux_distribution from './data/linux_distribution.json';
@@ -10,8 +13,11 @@ export const Charts = () => {
       {/* Bar Chart */}
       <BarChart data={world_population} />
 
-      {/* Tree Chart */}8
+      {/* Tree Chart */}
       <TreeChart data={linux_distribution} />
+
+      {/* TreeMap Chart */}
+      <TreeMapChart data={linux_distribution} />
     </div>
   );
 };
