79b4852baaf81f78087dde7b5f4ff98460053a87
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2021-07-26 08:43:43 +0000

    Add Linux distribution tree

--- src/components/charts/index.jsx
+++ src/components/charts/index.jsx
@@ -1,14 +1,17 @@
-import {BarChart} from './bar/'
-import world_population from './data/world_population'
+import { BarChart } from './bar/';
+import { TreeChart } from './map/Tree';
+import world_population from './data/world_population';
+import world_migrants from './data/world_migrants';
+import linux_distribution from './data/linux_distribution.json';
 
 export const Charts = () => {
-  
-
-
   return (
     <div>
       {/* Bar Chart */}
       <BarChart data={world_population} />
+
+      {/* Tree Chart */}8
+      <TreeChart data={linux_distribution} />
     </div>
-  )
-}
+  );
+};
